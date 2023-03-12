corrected elbo computation, optimised

src/utils.py
@@ -52,7 +52,6 @@ def compute_elbo(
         _alpha_: tr.Tensor,
         _eta_: tr.Tensor,
         w_ct: tr.Tensor, 
-        docs: np.ndarray,
     ) -> float: 
 
     """Compute the approximated value for the ELBO, which is the objective function in EM steps, against a batch of documents 
@@ -98,7 +97,7 @@ def compute_elbo(
     corpus_term =  eblo_corpus_part(_eta_, _lambda_, _alpha_, n_docs)
     #print(corpus_term)
 
-    doc_term = elbo_doc_depend_part(_alpha_, _gamma_, _phi_, _lambda_, w_ct, docs)
+    doc_term = elbo_doc_depend_part(_alpha_, _gamma_, _phi_, _lambda_, w_ct,)
     #print(doc_term)
 
     return corpus_term + doc_term
@@ -139,10 +138,9 @@ def eblo_corpus_part(
 def elbo_doc_depend_part(
         _alpha_: tr.Tensor, 
         _gamma_: tr.Tensor, 
-        _phi_: np.ndarray,
+        _phi_: tr.Tensor,
         _lambda_:tr.Tensor,
         w_ct:tr.Tensor, 
-        docs:np.ndarray,
     ) -> float:
 
     #print(w_ct)
@@ -166,61 +164,37 @@ def elbo_doc_depend_part(
     for k in range(K): 
         expec_beta_store[k] = expec_log_dirichlet(_lambda_[k])
 
-    term1, term2, term3 = 0, 0, 0  
+    term1, term2, term3 = 0, 0,0
     for d in range(M): 
 
         term1 -= log_gamma_sum_term(_gamma_[d])
 
+        expect_theta_d = expec_log_dirichlet(_gamma_[d])
         term1 += tr.dot(
             _alpha_ - _gamma_[d],
-            expec_log_dirichlet(_gamma_[d])
-        )
+            expect_theta_d,
+        ).item()
 
-        #print(term1)
+        for v in range(V): 
 
-        #get number of words, as per documnet 
-        _phi_d = tr.from_numpy(np.array(_phi_[d],dtype=float))
-        _phi_d = _phi_d.double()
-        Nd = _phi_d.shape[0] 
+            if w_ct[v][d] == 0: 
+                assert _phi_[d][v].sum() == 0
+                continue
 
-        #print(f"Optimised Function | Numnber of words {Nd}")
+            term2 += (w_ct[v][d]*tr.dot(_phi_[d][v], expect_theta_d)).item()
 
-        for n in range(Nd): 
+            if _phi_[d][v].sum() == 0: 
+                logs = tr.zeros(_phi_[d][v].shape, dtype=DTYPE)
+            else:
+                logs = tr.log(_phi_[d][v])
 
-            term2 += tr.dot(
-                _phi_d[n],
-                expec_log_dirichlet(_gamma_[d])
-            )
-
-            term2 -= tr.dot(
-                _phi_d[n],
-                tr.log(_phi_d[n])
-            )
-
-        for v in range(V):  
             
-            docs_d = tr.from_numpy(np.array(docs[d])).double()
-            vdoc_poss = (docs_d == v).nonzero(as_tuple=True)[0]
-            # print(vdoc_poss)
-            # print(_phi_d[vdoc_poss.tolist()])
-            # print(_phi_d[vdoc_poss.tolist()].unique(dim=0, return_counts=True))
+            term2 -= (w_ct[v][d]*tr.dot(_phi_[d][v], logs)).item()
+            
+            term3 += (w_ct[v][d]*tr.dot(_phi_[d][v], expec_beta_store[:,v])).item()
 
-            if len(vdoc_poss) == 0: 
-                assert w_ct[v][d] == 0, print(w_ct[v][d])
-                _phi_dn = tr.tensor([0]*K, dtype=DTYPE)
-            else: 
-                assert (_phi_d[vdoc_poss.tolist()].unique(dim = 0, return_counts=True)[1] == len(vdoc_poss)).all(), print(_phi_d[vdoc_poss.tolist()], vdoc_poss)
-                _phi_dn = _phi_d[vdoc_poss[0]]
-
-            #if vdoc_poss
-            #print(d, v, _phi_dn, w_ct[v][d])
-            term3 += w_ct[v][d] * tr.dot(
-                _phi_dn,
-                expec_beta_store[:,v]
-            )
-        #print(val_delta)
     #print(term1, term2, term3)
-    return term1.item() + term2.item() + term3.item() 
+    return term1 + term2 + term3
 
 
 def get_vocab_from_docs(docs:List[List[str]]): 

test/test_var_inf_elbo.py
@@ -192,7 +192,7 @@ def var_inf_part1_doc_level_mirror(
 
         #print(val_delta)
 
-    #print(term1+term4, term2+term5, term3)
+    print(term1+term4, term2+term5, term3)
 
     return term1 + term2 + term3 + term4 + term5 
 
@@ -224,7 +224,7 @@ def input3_():
                     [0.3, 0.3, 0.4], # document all words is at first vocab
                 ],
                 [
-                    [0.2, 0.2, 0.8], # document words is at the first 8 vocabs
+                    [0.3, 0.3, 0.4], # document words is at the first 8 vocabs
                     [0.2, 0.2, 0.8],    
                     [0.2, 0.2, 0.8],
                     [0.2, 0.2, 0.8], 
@@ -234,6 +234,34 @@ def input3_():
                     [0.2, 0.2, 0.8],
                 ],
             ], dtype=object
+        ),
+        '_phi_v':np.array(
+            [
+                [
+                    [0.3,0.3,0.4],#0
+                    [0.0,0.0,0.0],#1
+                    [0.0,0.0,0.0],#2
+                    [0.0,0.0,0.0],#3
+                    [0.0,0.0,0.0],#4
+                    [0.0,0.0,0.0],#5
+                    [0.0,0.0,0.0],#6
+                    [0.0,0.0,0.0],#7
+                    [0.0,0.0,0.0],#8
+                    [0.0,0.0,0.0],#9
+                ],
+                [
+                    [0.3,0.3,0.4],#0
+                    [0.2,0.2,0.8],#1
+                    [0.2,0.2,0.8],#2
+                    [0.2,0.2,0.8],#3
+                    [0.2,0.2,0.8],#4
+                    [0.2,0.2,0.8],#5
+                    [0.2,0.2,0.8],#6
+                    [0.2,0.2,0.8],#7
+                    [0.0,0.0,0.0],#8
+                    [0.0,0.0,0.0],#9
+                ],
+            ], dtype=float
         ),
         '_gamma_':np.array(
             [
@@ -248,8 +276,8 @@ def input3_():
         ]),
         'docs': np.array(
             [
-                [0,0,0,0,0,],
-                [0,1,2,3,4,5,6,7,],
+                [0,0,0,0,0],
+                [0,1,2,3,4,5,6,7],
             ], dtype=object
         ),
         'w_ct':np.array(
@@ -317,13 +345,12 @@ def test_docs_part_ELBO(input3_):
     doc_elbo = elbo_doc_depend_part(
         tr.from_numpy(input3_['_alpha_']),
         tr.from_numpy(input3_['_gamma_']),
-        input3_['_phi_'],
+        tr.from_numpy(input3_['_phi_v']),
         tr.from_numpy(input3_['_lambda_']),
         tr.from_numpy(input3_['w_ct']),
-        input3_['docs'],
     )
 
-    assert doc_elbo_mirror == doc_elbo
+    assert round(doc_elbo_mirror,5) == round(doc_elbo,5)
 
 
     corpus_eblo_mirror = var_inf_part2_corpus_level_mirror(
@@ -336,12 +363,11 @@ def test_docs_part_ELBO(input3_):
 
     elbo = compute_elbo(
         tr.from_numpy(input3_['_gamma_']),
-        input3_['_phi_'],
+        tr.from_numpy(input3_['_phi_v']),
         tr.from_numpy(input3_['_lambda_']),
         tr.from_numpy(input3_['_alpha_']),
         tr.from_numpy(input3_['_eta_']),
         tr.from_numpy(input3_['w_ct']),
-        input3_['docs'],
     )
 
     elbo_mirror = doc_elbo_mirror + corpus_eblo_mirror